e8fd262a9a80224aa1e69e3e96015db9c404e7ef
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-08-28 15:54:48 +0500

    Update register_screen.dart

--- lib/register_screen.dart
+++ lib/register_screen.dart
@@ -1,7 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:intl_phone_number_input/intl_phone_number_input.dart';
+// import 'package:intl_phone_number_input/intl_phone_number_input.dart';
 import 'package:password_bucket_private/states/current_user.dart';
+import 'package:phone_number/phone_number.dart' as phone;
 import 'package:provider/provider.dart';
 import 'value_notifiers.dart';
 import 'package:password_bucket_private/passwords_functions.dart' as functions;
@@ -18,7 +19,12 @@ class _RegisterScreenState extends State<RegisterScreen> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
   final confirmPasswordController = TextEditingController();
-  PhoneNumber number = PhoneNumber(isoCode: 'PK');
+  final phoneNumberController = TextEditingController();
+
+  // PhoneNumber number = PhoneNumber(isoCode: 'PK');
+
+  final ValueNotifier<bool> isPhoneNumberValidNotifier =
+      ValueNotifier<bool>(true);
 
   final ValueNotifier<bool> showPasswordNotifier = ValueNotifier<bool>(true);
   final ValueNotifier<bool> showConfirmPasswordNotifier =
@@ -41,7 +47,19 @@ class _RegisterScreenState extends State<RegisterScreen> {
     emailController.dispose();
     passwordController.dispose();
     confirmPasswordController.dispose();
+    phoneNumberController.dispose();
     super.dispose();
+  }
+
+  validatePhoneNumber({required String phoneNumber}) async {
+    // phone.PhoneNumber parsedPhoneNumber =
+    //     await phone.PhoneNumberUtil().parse(phoneNumber);
+    try {
+      isPhoneNumberValidNotifier.value =
+          await phone.PhoneNumberUtil().validate(phoneNumber);
+    } on Exception catch (e) {
+      isPhoneNumberValidNotifier.value = false;
+    }
   }
 
   void _register(context) async {
@@ -87,6 +105,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
       appBar: AppBar(
         title: const Text('Register'),
       ),
+      backgroundColor: Theme.of(context).primaryColor,
       body: LayoutBuilder(
         builder: (BuildContext context, BoxConstraints constraints) {
           return SingleChildScrollView(
@@ -112,8 +131,30 @@ class _RegisterScreenState extends State<RegisterScreen> {
                           },
                           keyboardType: TextInputType.name,
                           textCapitalization: TextCapitalization.words,
-                          decoration: const InputDecoration(
+                          decoration: InputDecoration(
+                            fillColor: Colors.white,
+                            filled: true,
                             hintText: 'Full Name',
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              borderSide: const BorderSide(
+                                  color: Colors.white, width: 2),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              borderSide: const BorderSide(
+                                  color: Colors.white, width: 2),
+                            ),
+                            errorBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              borderSide:
+                                  const BorderSide(color: Colors.red, width: 2),
+                            ),
+                            focusedErrorBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              borderSide:
+                                  const BorderSide(color: Colors.red, width: 2),
+                            ),
                           ),
                           controller: fullNameController,
                           validator: (value) {
@@ -126,22 +167,84 @@ class _RegisterScreenState extends State<RegisterScreen> {
                       ),
                       const Spacer(),
                       Padding(
-                        padding: const EdgeInsets.fromLTRB(10, 1, 10, 1),
-                        child: Center(
-                          child: InternationalPhoneNumberInput(
-                            initialValue: number,
-                            onInputChanged: (PhoneNumber value) {
-                              phoneNumberNotifier.value = value.toString();
-                            },
-                          ),
-                        ),
-                      ),
+                          padding: const EdgeInsets.fromLTRB(10, 1, 10, 1),
+                          child: ValueListenableBuilder(
+                              valueListenable: isPhoneNumberValidNotifier,
+                              builder: (context, bool validPhoneNumber, _) {
+                                return TextFormField(
+                                  keyboardType: TextInputType.phone,
+                                  decoration: InputDecoration(
+                                    fillColor: Colors.white,
+                                    filled: true,
+                                    hintText: 'Phone Number: +xx xxx xxxxxxx',
+                                    enabledBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(20),
+                                      borderSide: const BorderSide(
+                                          color: Colors.white, width: 2),
+                                    ),
+                                    focusedBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(20),
+                                      borderSide: const BorderSide(
+                                          color: Colors.white, width: 2),
+                                    ),
+                                    errorBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(20),
+                                      borderSide: const BorderSide(
+                                          color: Colors.red, width: 2),
+                                    ),
+                                    focusedErrorBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(20),
+                                      borderSide: const BorderSide(
+                                          color: Colors.red, width: 2),
+                                    ),
+                                  ),
+                                  controller: phoneNumberController,
+                                  onChanged: (text) {
+                                    phoneNumberNotifier.value = text;
+                                    validatePhoneNumber(phoneNumber: text);
+                                  },
+                                  validator: (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return 'Enter a phone number!';
+                                    }
+                                    if (!RegExp("^[+]").hasMatch(value)) {
+                                      return "Enter phone number with area code";
+                                    }
+                                    if (!validPhoneNumber) {
+                                      return "Enter a valid phone number";
+                                    }
+                                    return null;
+                                  },
+                                );
+                              })),
                       const Spacer(),
                       Padding(
                         padding: const EdgeInsets.fromLTRB(10, 1, 10, 1),
                         child: TextFormField(
-                          decoration: const InputDecoration(
+                          decoration: InputDecoration(
+                            fillColor: Colors.white,
+                            filled: true,
                             hintText: 'Email',
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              borderSide: const BorderSide(
+                                  color: Colors.white, width: 2),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              borderSide: const BorderSide(
+                                  color: Colors.white, width: 2),
+                            ),
+                            errorBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              borderSide:
+                                  const BorderSide(color: Colors.red, width: 2),
+                            ),
+                            focusedErrorBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              borderSide:
+                                  const BorderSide(color: Colors.red, width: 2),
+                            ),
                           ),
                           keyboardType: TextInputType.emailAddress,
                           controller: emailController,
@@ -174,16 +277,41 @@ class _RegisterScreenState extends State<RegisterScreen> {
                                       passwordNotifier.value = text;
                                     },
                                     decoration: InputDecoration(
-                                        hintText: 'Password',
-                                        suffixIcon: passwordText.isNotEmpty
-                                            ? IconButton(
-                                                onPressed: () => functions
-                                                    .togglePasswordVisibility(
-                                                        showPasswordNotifier),
-                                                icon: Icon(showPassword
-                                                    ? Icons.visibility
-                                                    : Icons.visibility_off))
-                                            : null),
+                                      hintText: 'Password',
+                                      suffixIcon: passwordText.isNotEmpty
+                                          ? IconButton(
+                                              onPressed: () => functions
+                                                  .togglePasswordVisibility(
+                                                      showPasswordNotifier),
+                                              icon: Icon(
+                                                  color: Colors.grey,
+                                                  showPassword
+                                                      ? Icons.visibility
+                                                      : Icons.visibility_off))
+                                          : null,
+                                      fillColor: Colors.white,
+                                      filled: true,
+                                      enabledBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(20),
+                                        borderSide: const BorderSide(
+                                            color: Colors.white, width: 2),
+                                      ),
+                                      focusedBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(20),
+                                        borderSide: const BorderSide(
+                                            color: Colors.white, width: 2),
+                                      ),
+                                      errorBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(20),
+                                        borderSide: const BorderSide(
+                                            color: Colors.red, width: 2),
+                                      ),
+                                      focusedErrorBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(20),
+                                        borderSide: const BorderSide(
+                                            color: Colors.red, width: 2),
+                                      ),
+                                    ),
                                     controller: passwordController,
                                     obscureText: showPassword,
                                     validator: (value) {
@@ -218,6 +346,32 @@ class _RegisterScreenState extends State<RegisterScreen> {
                                       confirmPasswordNotifier.value = text;
                                     },
                                     decoration: InputDecoration(
+                                        fillColor: Colors.white,
+                                        filled: true,
+                                        enabledBorder: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(20),
+                                          borderSide: const BorderSide(
+                                              color: Colors.white, width: 2),
+                                        ),
+                                        focusedBorder: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(20),
+                                          borderSide: const BorderSide(
+                                              color: Colors.white, width: 2),
+                                        ),
+                                        errorBorder: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(20),
+                                          borderSide: const BorderSide(
+                                              color: Colors.red, width: 2),
+                                        ),
+                                        focusedErrorBorder: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(20),
+                                          borderSide: const BorderSide(
+                                              color: Colors.red, width: 2),
+                                        ),
                                         hintText: 'Confirm Password',
                                         suffixIcon: confirmPasswordText
                                                 .isNotEmpty
@@ -225,9 +379,11 @@ class _RegisterScreenState extends State<RegisterScreen> {
                                                 onPressed: () => functions
                                                     .togglePasswordVisibility(
                                                         showConfirmPasswordNotifier),
-                                                icon: Icon(showConfirmPassword
-                                                    ? Icons.visibility
-                                                    : Icons.visibility_off))
+                                                icon: Icon(
+                                                    color: Colors.grey,
+                                                    showConfirmPassword
+                                                        ? Icons.visibility
+                                                        : Icons.visibility_off))
                                             : null),
                                     controller: confirmPasswordController,
                                     obscureText: showConfirmPassword,
@@ -253,7 +409,17 @@ class _RegisterScreenState extends State<RegisterScreen> {
                       const Spacer(),
                       ValueListenableBuilder(
                         builder: (BuildContext context, bool disableButton, _) {
-                          return ElevatedButton(
+                          return SizedBox(
+                            width: 200,
+                            height: 45,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(30.0),
+                                ),
+                                primary:
+                                    const Color.fromRGBO(63, 125, 217, 1.0),
+                              ),
                               onPressed: disableButton
                                   ? null
                                   : () {
@@ -261,7 +427,12 @@ class _RegisterScreenState extends State<RegisterScreen> {
                                         _register(context);
                                       }
                                     },
-                              child: const Text('Register'));
+                              child: const Text(
+                                'Register',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ),
+                          );
                         },
                         valueListenable: disableRegisterButtonNotifier,
                       ),
