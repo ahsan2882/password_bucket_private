96f796fb29588165eedb080bfbf0a41a47b7839d
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-08-28 13:38:12 +0500

    Update current_user.dart

--- lib/states/current_user.dart
+++ lib/states/current_user.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 import '../value_notifiers.dart';
 
@@ -15,10 +16,10 @@ class CurrentUser extends ChangeNotifier {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
 
   Future<bool> signUpUser(
-      {name: String,
-      phoneNumber: String,
-      email: String,
-      password: String}) async {
+      {required String name,
+      required String phoneNumber,
+      required String email,
+      required String password}) async {
     bool registerStatus = false;
     final user = <String, String>{
       'name': name,
@@ -30,23 +31,47 @@ class CurrentUser extends ChangeNotifier {
           .createUserWithEmailAndPassword(email: email, password: password);
       if (userCredentials.user != null) {
         registerStatus = true;
+        userCredentials.user!.updateDisplayName(name);
         _db
             .collection('users')
-            .doc(userCredentials.user?.uid)
+            .doc(userCredentials.user?.email)
             .set(user)
             .onError((error, _) => print('Error writing document: $error'));
       }
-    } catch (e) {
+    } on FirebaseAuthException catch (e) {
       registerStatus = false;
-      showMessagePopup.value = true;
-      int startIndex = e.toString().indexOf(']');
-      showMessagePopupText.value =
-          e.toString().substring(startIndex + 1).trim();
-      disableRegisterButton.value = false;
-      await Future.delayed(const Duration(seconds: 5));
-      showMessagePopup.value = false;
-      showMessagePopupText.value = '';
+      showMessagePopupTextNotifier.value = e.message.toString();
+      disableRegisterButtonNotifier.value = false;
     }
     return registerStatus;
+  }
+
+  Future<bool> loginWithEmail(
+      {required String email, required String password}) async {
+    bool loginStatus = false;
+
+    try {
+      UserCredential userCredentials = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      if (userCredentials.user != null) {
+        loginStatus = true;
+        _uid = userCredentials.user!.uid;
+      }
+    } on FirebaseAuthException catch (e) {
+      loginStatus = false;
+      showMessagePopupTextNotifier.value = e.message.toString();
+    }
+    return loginStatus;
+  }
+
+  Future<UserCredential> loginWithGoogle() async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+    return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 }
